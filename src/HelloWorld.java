fd8d76ae9606cc9b623632dccc7e342cb27a437c
Nazım-PC <nzm.eryilmaz@gmail.com>
2022-07-06 11:51:39 +0300

    Third Try

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("How're you?");
+        System.out.println("What's up?");
     }
 }
